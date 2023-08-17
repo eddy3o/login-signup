286b9a82463101cbfc4c4e04d85dd40e66406099
eddy3o <mauricio.gonsales.23@outlook.com>
2023-08-17 16:36:48 -0600

    allowed hosts

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 DEBUG = False
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://login-register-kt00.onrender.com/']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
